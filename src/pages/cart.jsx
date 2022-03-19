5350f06b2d035b207c014937b11518908a4cfba1
Samuel Claudino <samuelcs131@gmail.com>
2022-03-19 18:25:02 -0300

    ajuste mobile pagina carrinho

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -2,9 +2,8 @@ import HeadPage from "../components/HeadPage";
 import Menu from "../components/Menu";
 import DataCars from "../../public/DataCars.json"
 import { CardCarImage } from "../styles/style";
+import Footer from "../components/Footer"
 
-
- 
 const Cart = () => {
     return ( 
         <>
@@ -16,35 +15,38 @@ const Cart = () => {
                     <span>You have {2} cars in your cart</span>
                 </div>
 
-            {DataCars.length !== 0 ? ( DataCars.map( (car, index) =>{return(
-                <div key={index} className="card-car">
-                        <div className="head-card-car">
-                            <span>April 14</span>
-                        </div>
-                    <hr />
-                    <div className="container-card-car">
-                        <div className="container-image-and-info">
-                            <CardCarImage image={car.images.model}/>
-                        
-                            <div className="info-car-group">
-                                <h1>{car.model}</h1>
-                                <p>{car.versions[1].name}</p>
-                                <p>Color: {car.color[0].name}</p>
-                                <span className="value-car">${car.versions[0].price}</span>
+                <div className="card-container"> 
+                    {DataCars.length !== 0 ? ( DataCars.map( (car, index) =>{return(
+                        <div key={index} className="card-car">
+                                <div className="head-card-car">
+                                    <span>April 14</span>
+                                </div>
+                            <hr />
+                            <div className="container-card-car">
+                                <div className="container-image-and-info">
+                                    <CardCarImage image={car.images.model}/>
+                                
+                                    <div className="info-car-group">
+                                        <h1>{car.model}</h1>
+                                        <p>{car.versions[1].name}</p>
+                                        <p>Color: {car.color[0].name}</p>
+                                        <span className="value-car">${car.versions[0].price}</span>
+                                    </div>
+                                </div>
+                                <div className="btn-group">
+                                    <button className="btn-remove">Remove</button>
+                                    <button className="btn-finalize">Finalize</button>
+                                </div>
                             </div>
                         </div>
-                        <div className="btn-group">
-                            <button className="btn-remove">Remove</button>
-                            <button className="btn-finalize">Finalize</button>
-                        </div>
-                    </div>
-                </div>
-            )}) 
-            )
-            : 
-            (<h1>Vazio</h1>)}
+                    )}) 
+                    )
+                    : 
+                    (<h1>Vazio</h1>)}
+            </div>
 
             </section>
+            <Footer/>
         </>
      );
 }
