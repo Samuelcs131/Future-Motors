e86c998e6a68c04354e534e9961f2983c458f2c9
Samuel Claudino <samuelcs131@gmail.com>
2022-03-24 05:35:47 -0300

    att

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    "BASE_URL": "http://localhost:3000/",
-    "MONGODB_URL":"mongodb+srv://futuremotors:5Nz6as8fvAwfa4q1@cluster0.xvqoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  }
-}
